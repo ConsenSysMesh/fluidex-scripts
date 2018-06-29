a48d7c5b01468f5622bd375ffbf821a1e30b3a0a
Ernani São Thiago <ernanirst@gmail.com>
2018-06-28 22:08:55 -0300

    updated ci image to setup kubectl

---
+++ docker/fluidex-ci-agent/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ -z "$KUBERNETES_SERVER" ]; then
+    echo "Missing env variable KUBERNETES_SERVER"
+    exit 1
+fi
+
+if [ -z "$KUBERNETES_TOKEN" ]; then
+    echo "Missing env variable KUBERNETES_TOKEN"
+    exit 1
+fi
+
+if [ -z "$KUBERNETES_CERT" ]; then
+    echo "Missing env variable KUBERNETES_CERT"
+    exit 1
+fi
+
+if [ -z "$KUBERNETES_TEMPLATE" ]; then
+    echo "Missing env variable KUBERNETES_TEMPLATE"
+    exit 1
+fi
+
+mkdir -p /root/.kube
+
+KUBERNETES_CERT_PATH=/root/.kube/ca.crt
+echo -e "$KUBERNETES_CERT" > "$KUBERNETES_CERT_PATH"
+
+kubectl config set-cluster default --server=$KUBERNETES_SERVER --certificate-authority=$KUBERNETES_CERT_PATH
+kubectl config set-context default --cluster=default
+kubectl config set-credentials admin --token=$KUBERNETES_TOKEN
+kubectl config set-context default --user=admin
+kubectl config use-context default
